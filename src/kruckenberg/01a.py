f74225fa4915f7ebec30a3660541e42c71ac4dd8
Nick Kruckenberg <nkruckenberg@gmail.com>
2024-12-03 20:43:23 +0000

    TEMP

---
+++ src/kruckenberg/01a.py
@@ -0,0 +1,12 @@
+# read in the file
+from utils.files import readlines
+lines = readlines('01.txt')
+print(lines)
+
+# split the columns into lists
+
+# sort the lists
+
+# find the difference for each pair
+
+# add the differences
